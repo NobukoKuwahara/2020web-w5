8110baf4611cdf9aad1a3b7214efab21e0bb0ba5
NobukoKuwahara <75403382+NobukoKuwahara@users.noreply.github.com>
2020-12-17 14:25:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+//let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText = '気温 華氏86度で外はとても暑かった。:insertx: は歩いて出かけた。 :inserty:に着いたとき、:insertz:。体重90ポンドの:insertx:は驚いたが、Bobは驚かなかった。:inserty:ではよくあることだった。';
+let insertX = ['アンパンマン','ドラえもん','ミッキーマウス'];
+let insertY = ['小平市','村','ディズニーランド'];
+let insertZ = ['人がたくさんいて混雑していた','雷がなっていた','奇妙な動物に遭遇した'];
 
 randomize.addEventListener('click', result);
 
@@ -32,10 +33,10 @@ function result() {
   }
 
   if(document.getElementById("日本").checked) {
-    const weight = Math.round(300*0.0714286) + ' stone';
-    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
-    newStory = newStory.replace('94 fahrenheit',temperature);
-    newStory = newStory.replace('300 pounds',weight);
+    const weight = Math.round(90/2.2024) + 'kg';
+    const temperature = '摂氏' + Math.round((86-32) * 5 / 9);
+    newStory = newStory.replace('華氏86',temperature);
+    newStory = newStory.replace('90ポンド',weight);
   }
 
   story.textContent = newStory;
